667e108a005e17b3ad4a5ce448a321ff3bb49c7c
AayushPurivsKartik <aayushpuri683@gmail.com>
2024-08-06 16:55:46 +0530

    add new project

---
+++ weather-app/src/background.jsx
@@ -0,0 +1,10 @@
+import './backgr.css'
+export default function Background(){
+    return (
+        <>
+        <div class="bg"></div>
+<div class="bg bg2"></div>
+<div class="bg bg3"></div>
+        </>
+    )
+}
